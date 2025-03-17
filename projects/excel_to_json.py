7cba57b3bef997a4ee7a72e409852067285aa601
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2025-03-17 19:20:43 +0530

    Add files via upload

---
+++ projects/excel_to_json.py
@@ -0,0 +1,11 @@
+
+#import the pandas library
+
+import pandas as pd
+
+# Read the excel file
+df = pd.read_excel('europe.xlsx')
+
+
+# Convert the dataframe to json
+df.to_json('europe.json', orient='records')
